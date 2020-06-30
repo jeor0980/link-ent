003fc069a76e9c85edf0c791cd2a25b3d228f7a1
jeor0980 <jesusotovar@hotmail.com>
2020-06-29 23:52:50 -0600

    Jesus Version Initial Commit

--- components/Header.js
+++ components/Header.js
@@ -3,9 +3,9 @@ function Header() {
   return (
     <header className="container flex text-center mx-auto py-10">
       <div className="md:w-4/6 mx-auto">
-        <img src="https://placekitten.com/200/200" className="h-24 rounded-full mx-auto mb-5" />
-        <h1 className="font-bold">Your Name</h1>
-        <p>Your Title</p>
+        <img src="/profilepic.jpg" className="h-24 rounded-full mx-auto mb-5" />
+        <h1 className="font-bold">Jesus Ortiz</h1>
+        <p>Jesus is a coder by day and bass power chord strummer by night, he also often likes to stay up late going to live rock shows.</p>
       </div>
     </header>
   );
